2e1a68fe1597edea72da0d11b3e71610098c3ddc
SamarthGithub2020 <69111210+SamarthGithub2020@users.noreply.github.com>
2025-07-23 19:24:49 -0400

    test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_conn():
     response=app.test_client().get("/")
     
     assert response.status_code==200
-    assert response.data.decode("utf-8")=="WELCOME TO THE HOME PAGE!!"
+    assert response.data==b"HOME PAGE"
